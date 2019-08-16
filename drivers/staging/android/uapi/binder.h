0496466c9515ccfec679e114ae53a1d91ff03017
Vaisakh Murali <vaisakhmurali@gmail.com>
2019-08-16 23:39:15 +0530

    include: binder: Remove redefinition
    * Lot of merges cause these.
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- drivers/staging/android/uapi/binder.h
+++ drivers/staging/android/uapi/binder.h
@@ -94,14 +94,6 @@ enum flat_binder_object_flags {
 	 * scheduling policy from the caller (for synchronous transactions).
 	 */
 	FLAT_BINDER_FLAG_INHERIT_RT = 0x800,
-
-	/**
-	 * @FLAT_BINDER_FLAG_TXN_SECURITY_CTX: request security contexts
-	 *
-	 * Only when set, causes senders to include their security
-	 * context
-	 */
-	FLAT_BINDER_FLAG_TXN_SECURITY_CTX = 0x1000,
 };
 
 #ifdef BINDER_IPC_32BIT
